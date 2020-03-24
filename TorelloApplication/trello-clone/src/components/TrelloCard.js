23188970a079e4239b9511dd28c3f10d4bcb31b1
Roden Artuyo <roden.artuyo@sun-asterisk.com>
2020-03-24 11:50:11 +0800

    [Trello-Clone] Part 01 - Project Setup

---
+++ TorelloApplication/trello-clone/src/components/TrelloCard.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Card } from "react-bootstrap";
+
+const TrelloCard = () => {
+  return (
+    <Card>
+      <Card.Body>
+        This is some text within a card body.
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default TrelloCard;
