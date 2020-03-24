21ed5c26820978b6dfc1b3bc89a35d91756de6a7
Roden Artuyo <roden.artuyo@sun-asterisk.com>
2020-03-24 13:02:59 +0800

    [Step 04] Connect State in your component using connect function

--- TorelloApplication/trello-clone/src/components/App.js
+++ TorelloApplication/trello-clone/src/components/App.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import TrelloList from './TrelloList.js';
+import { connect } from 'react-redux';
 
 class App extends React.Component{
 
   render(){
+    const { lists } = this.props
     return (
       <div className="App">
         <h2>Trello Clone</h2>
-        <TrelloList title="test" />
+        { lists.map(list => <TrelloList title={list.title} cards={list.cards}/>)}
       </div>
     )
   }
 }
 
-export default App;
+const mapStateFromProps = state => ({
+  lists: state.lists
+});
+
+export default connect(mapStateFromProps)(App);
