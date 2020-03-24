79c587090c09e1e787e6ea228eb805d1f8bcc247
rodenartuyo <52144255+rodenartuyo@users.noreply.github.com>
2020-03-24 14:37:24 +0800

    [Trello-Clone] Part 02 - List and Card Component (#2)
    * [Step 01]Create listReducer.js
    
    * [Step 02]Import listReducer.js in reducers/index.js
    
    * [Step 03] Import reducers in store/index.js
    
    * [Step 04] Connect State in your component using connect function
    
    * [Step 05] Add Designs to Cards and Lists

--- TorelloApplication/trello-clone/src/components/App.js
+++ TorelloApplication/trello-clone/src/components/App.js
@@ -1,16 +1,38 @@
 import React from 'react';
 import TrelloList from './TrelloList.js';
+import { connect } from 'react-redux';
 
 class App extends React.Component{
 
   render(){
+    const { lists } = this.props
     return (
       <div className="App">
         <h2>Trello Clone</h2>
-        <TrelloList title="test" />
+        <div style={ styles.listsContainer }>
+          { lists.map(list => 
+            <TrelloList 
+              title={list.title} 
+              cards={list.cards} 
+              key={list.id}
+            />
+          )}
+        </div>
       </div>
     )
   }
 }
 
-export default App;
+const styles = {
+  listsContainer: {
+    display: "flex",
+    flexDirection: "row",
+    marginRight:8
+  }
+};
+
+const mapStateFromProps = state => ({
+  lists: state.lists
+});
+
+export default connect(mapStateFromProps)(App);
---
+++ TorelloApplication/trello-clone/src/reducers/listsReducer.js
@@ -0,0 +1,40 @@
+const initialState = [
+  {
+    title: "To Do",
+    id: 0,
+    cards: [
+      {
+        id: 0,
+        text: "Setup Project"
+      },
+      {
+        id: 1,
+        text: "Install necessary Packages"
+      }
+    ]
+  },
+  {
+    title: "In Progress",
+    id: 1,
+    cards: [
+      {
+        id: 0,
+        text: "ERD Design"
+      },
+      {
+        id: 1,
+        text: "Install necessary Packages"
+      }
+    ]
+  }
+]
+
+const listsReducer = (state = initialState, action) => {
+  switch (action.type) {
+    default: 
+      return state;
+  }
+}
+
+
+export default listsReducer;
