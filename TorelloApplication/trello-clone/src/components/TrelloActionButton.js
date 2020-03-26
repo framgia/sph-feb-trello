79f66e9bab83de532064a8f20f4fd855b9f08792
rodenartuyo <52144255+rodenartuyo@users.noreply.github.com>
2020-03-26 10:17:34 +0800

    [TRELLO-Clone][Part 03] Reusable Component (#3)
    * [TRELLO-Clone][Part 03] Reusable Component
    
    * Change attribute class to classNAme

---
+++ TorelloApplication/trello-clone/src/components/TrelloActionButton.js
@@ -0,0 +1,137 @@
+import React from "react";
+import TextareaAutosize from 'react-textarea-autosize';
+import { Card } from "react-bootstrap";
+import { Button } from "react-bootstrap";
+
+class TrelloActionButton extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+        formOpen: false
+    }
+  }
+  renderAddButton = () => {
+    const { list } =  this.props;
+
+    const buttonText = list ? "Add another list" : "Add another card";
+    const buttonTextOpacity = list ? 1 : 0.5;
+    const buttonTextColor = list ? "white" : "inherit";
+    const buttonTextBackground = list ? "rgba(0,0,0,0.15)" : "inherit";
+
+    return (
+      <div
+        onClick={this.openForm}
+        style={{
+          ...styles.openFormButtonGroup,
+          opacity: buttonTextOpacity,
+          color: buttonTextColor,
+          backgroundColor: buttonTextBackground,
+        }}
+      >
+        <i className="fa fa-plus"></i>
+        <span>
+          {buttonText}
+        </span>
+      </div>
+    );
+  }
+
+  openForm = () => {
+    this.setState({
+      formOpen: true,
+    });
+  }
+
+  closeForm = () => {
+    this.setState({
+      formOpen: false,
+      text: ""
+    });
+  }
+
+  handleInputChange = e => {
+    this.setState({
+      text: e.target.value
+    });
+  }
+
+  renderForm = () => {
+    const { list } = this.props;
+
+    const placeholder = list 
+      ? "Enter list title..."
+      : "Enter a title for this card...";
+
+    const buttonTitle = list ? "Add List" : "Add Card";
+
+    return (
+      <div>
+        <Card 
+          style={{  
+            overFlow: "visible",
+            minHeight: 80,
+            minWidth: 272,
+            padding: "6px 8px 2px"
+          }}
+        >
+          <TextareaAutosize
+            style={styles.formTextArea}
+            placeholder={placeholder}
+            onChange={this.handleInputChange}
+            onBlur={this.closeForm}
+            value={this.state.text}
+            autoFocus
+          />
+        </Card>
+        <div style={styles.formButtonGroup}>
+          <Button 
+            variant="success"
+          >
+            {buttonTitle}
+          </Button>
+          <i 
+            className="fa fa-times" 
+            style={{
+              fontSize: 20,
+              marginLeft: 4,
+              cursor: "pointer"
+            }}
+          ></i>
+        </div>
+      </div>
+    )
+  }
+
+  render() {
+    return this.state.formOpen ? this.renderForm() : this.renderAddButton();
+  }
+}
+
+const styles = {
+  openFormButtonGroup: {
+    display: "flex",
+    alignItems: "center",
+    cursor: "pointer",
+    borderRadius: 3,
+    width: 272,
+    height: 36,
+    paddingLeft: 30
+  },
+
+  formTextArea: {
+    resize: "none",
+    width: "100%",
+    overFlow: "hidden",
+    outline: "none",
+    border: "none",
+  },
+
+  formButtonGroup: {
+    marginTop: 8,
+    display: "flex",
+    alignItems: "center"
+  }
+}
+
+export default TrelloActionButton;
